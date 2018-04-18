58e822ed7f07bfa89fab603c28fc1a50f2d779d4
James Fairbairn <jamesaf112@gmail.com>
2018-04-18 22:34:46 +0100

    Fixing setup.bat to actually work and adding code-workspace file

--- setup.bat
+++ setup.bat
@@ -1,24 +1,21 @@
-REM create the python virtual environment
-echo "creating python virtual environment"
+@echo off
+:: create the python virtual environment
+echo creating python virtual environment
 python -m venv omnimuse-env
 IF %ERRORLEVEL% NEQ 0 GOTO SetupError
 
-REM activate the venv and install the requirements.txt, then deactivate the virtual environment
-echo "installing python packages in virtual environment"
-.\omnimuse-env\Scripts\activate
-pip install -r requirements.txt
-.\omnimuse-env\Scripts\deactivate.bat
+:: activate the venv and install the requirements.txt, then deactivate the virtual environment
+echo installing python packages in virtual environment
+cmd /c ".\omnimuse-env\Scripts\activate & pip install -r requirements.txt & .\omnimuse-env\Scripts\deactivate"
 IF %ERRORLEVEL% NEQ 0 GOTO SetupError
 
-REM install node packages
-echo "installing node packages"
-cd .\www\omnimuse\
-npm install
-cd .\..\..
+:: install node packages
+echo installing node packages
+cmd /c "cd .\www\omnimuse\ & npm install & cd .\..\.."
 IF %ERRORLEVEL% NEQ 0 GOTO SetupError
 
 exit /B 0
 
 :SetupError
-echo "Failed to run the setup file. Refer to the README file for further details on setup."
+echo Failed to run the setup file. Refer to the README file for further details on setup.
 exit /B 1
